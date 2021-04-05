7ce0ab73c1b6d416ad2be5c4362063abfd321aeb
Meepo <524545375@qq.com>
2021-04-05 19:35:56 +0800

    draw triangle without depth

--- MyRenderer/geometry.h
+++ MyRenderer/geometry.h
@@ -52,4 +52,8 @@ template <class t> std::ostream& operator<<(std::ostream& s, Vec3<t>& v) {
 	return s;
 }
 
+template<class T>
+Vec3<T> cross(const Vec3<T>& v1, const Vec3<T>& v2) {
+	return Vec3<T>(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
 #endif //__GEOMETRY_H__
--- MyRenderer/main.cpp
+++ MyRenderer/main.cpp
@@ -1,5 +1,7 @@
+#include<iostream>
 #include<vector>
 #include<cmath>
+#include<algorithm>
 #include "tgaimage.h"
 #include "geometry.h"
 #include "model.h"
@@ -7,8 +9,8 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 Model* model = nullptr;
-const int height = 800;
-const int width = 800;
+const int height = 200;
+const int width = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color) {
 	bool steep = false;
@@ -64,6 +66,37 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
 	}*/
 }
 
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+	Vec3f crs = cross(Vec3f(pts[1].x - pts[0].x, pts[2].x - pts[0].x, pts[0].x - P.x),
+		Vec3f(pts[1].y - pts[0].y, pts[2].y - pts[0].y, pts[0].y - P.y));
+	if (std::abs(crs.z) < 1)   // 意味着crs.z == 0，即三角形的边共线，返回一个负值即可
+		return Vec3f(-1, 1, 1);
+	return Vec3f(1.0f - crs.x / crs.z - crs.y / crs.z, crs.x / crs.z, crs.y / crs.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
+	// 找到bounding box 大小
+	Vec2i boundingBoxMin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i boundingBoxMax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; ++i) {
+		boundingBoxMax.x = std::min(clamp.x, std::max(boundingBoxMax.x, pts[i].x));
+		boundingBoxMax.y = std::min(clamp.x, std::max(boundingBoxMax.y, pts[i].y));
+		boundingBoxMin.x = std::max(0, std::min(boundingBoxMin.x, pts[i].x));
+		boundingBoxMin.y = std::max(0, std::min(boundingBoxMin.y, pts[i].y));
+	}
+	// 遍历bounding box的点
+	Vec2i Point;
+	for (Point.x = boundingBoxMin.x; Point.x <= boundingBoxMax.x; ++Point.x) {
+		for (Point.y = boundingBoxMin.y; Point.y <= boundingBoxMax.y; ++Point.y) {
+			Vec3f baryCor = barycentric(pts, Point);
+			if (baryCor.x < 0 || baryCor.y < 0 || baryCor.z < 0)
+				continue;  // 点在三角形外
+			image.set(Point.x, Point.y, color);
+		}
+	}
+}
+
 
 int main(int argc, char** argv) {
 	// read model
@@ -76,20 +109,19 @@ int main(int argc, char** argv) {
 	}
 	// set image's width/height/colortype
 	TGAImage image(width, height, TGAImage::RGB);
-	// draw line
+	// draw Model
 	for (int i = 0; i < model->nfaces(); ++i) {
 		std::vector<int> face = model->face(i);
+		Vec2i Points[3];
 		for (int j = 0; j < 3; ++j) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
+			Vec3f worldCoords = model->vert(face[j]);
+			Points[j] = Vec2i((worldCoords.x + 1) * width / 2, (worldCoords.y + 1) * height / 2);
 		}
+		triangle(Points, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 	}
-
+	// draw triangle
+	/*Vec2i pts[3] = { Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160) };
+	triangle(pts, image, red);*/
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	// write output
